b47c15bdd41bd618a9e46efeeb359b592365583d
Iván Ramírez <Ivan_SocialNetworks@hotmail.com>
2017-12-09 14:34:58 -0500

    footer
    Main page's footer

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.set('trust proxy', 1);
 app.use(helmet({
   contentSecurityPolicy: {
