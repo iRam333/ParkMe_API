2e155b32e69f7186c99d16470cd8f8534a92e771
Iván Ramírez <Ivan_SocialNetworks@hotmail.com>
2018-03-03 21:36:50 -0500

    console log

--- models/parkingBay.js
+++ models/parkingBay.js
@@ -2,7 +2,8 @@ var db = require('../dbconnection');
 
 var parkingBay = {
   updateParkingBay:function(parkingBay, callback) {
-    return db.query("UPDATE PARKING_BAY SET NUM_STATUS = ? WHERE NUM_LATITUDE = ? AND NUM_LONGITUDE = ?", [parkingBay.status, parkingBay.latitude, parkingBay.longitude], callback);
+    console.log(parkingBay);
+    return db.query("UPDATE PARKING_BAY SET NUM_STATUS = ? WHERE ID_PARKING_BAY = (SELECT ID_PARKING_BAY FROM PARKING_BAY WHERE NUM_LATITUDE = ? AND NUM_LONGITUDE = ?)", [parkingBay.status, parkingBay.latitude, parkingBay.longitude], callback);
   }
 };
 
